de8126a1491704ffbe33d59b5702a5299a857fee
Dave Greenwood <dgrnwd@gmail.com>
2020-03-09 18:24:08 +0000

    include batch in example

--- example.py
+++ example.py
@@ -5,6 +5,7 @@ import torch
 import matplotlib.pyplot as plt
 from PIL import Image
 
+from thinplatespline.batch import TPS
 from thinplatespline.tps import tps_warp
 from thinplatespline.utils import (
     TOTEN, TOPIL, grid_points_2d, noisy_grid, grid_to_img)
@@ -25,10 +26,32 @@ t1 = time.time()
 
 print(f"time: {t1-t0:0.3f}", "created variables")
 
+
+# %% batch testing
+
+tpsb = TPS(size=(h, w), device=DEVICE)
+
+t0 = time.time()
+warped_grid_b = tpsb(X[None, ...], Y[None, ...])
+t1 = time.time()
+
+ten_img = TOTEN(img)
+
+ten_wrp_b = torch.grid_sampler_2d(
+    ten_img[None, ...],
+    warped_grid_b,
+    0, 0, False)
+
+print(f"time: {t1-t0:0.3f}", "warped image")
+
+img_wrp_b = TOPIL(ten_wrp_b[0].cpu())
+
+
 # %%
 
 t0 = time.time()
 warped_grid = tps_warp(X, Y, dense_grid)
+t1 = time.time()
 
 ten_img = TOTEN(img)
 
@@ -37,19 +60,21 @@ ten_wrp = torch.grid_sampler_2d(
     warped_grid[None, None, :, :2],
     0, 0, False)[0, :, 0, :]
 
-t1 = time.time()
 print(f"time: {t1-t0:0.3f}", "warped image")
 
 img_wrp = TOPIL(ten_wrp.reshape(3, h, w).cpu())
 
 # %%
 
-fig, ax = plt.subplots(1, 2, figsize=[9, 7], sharey=True)
+fig, ax = plt.subplots(1, 3, figsize=[13, 7], sharey=True)
 ax[0].imshow(img)
 ax[0].plot(x1, y1, "+g", ms=15, mew=2, label="uniform")
 ax[0].legend(loc=1)
 ax[1].plot(x2, y2, "+r", ms=15, mew=2, label="target")
 ax[1].imshow(img_wrp)
 ax[1].legend(loc=1)
+ax[2].plot(x2, y2, "+r", ms=15, mew=2, label="batch")
+ax[2].imshow(img_wrp_b)
+ax[2].legend(loc=1)
 plt.tight_layout()
-fig.savefig("plot.jpg", bbox_inches="tight")
+# fig.savefig("plot.jpg", bbox_inches="tight")
